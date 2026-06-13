d6444733a103d27e755bee7f6f72d63cd6a7b863
Ezra K. <ln60914@mail.harpercollege.edu>
2026-06-12 20:36:40 -0500

    Wrote the Maximum in an Array program

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+/*#include <iostream>
 
 using namespace std;
 
@@ -15,4 +15,23 @@ int main() {
 
     cout << endl << "Dereferenced a: " << *ptr_a;
     cout << endl << "Dereferenced b: " << *ptr_b;
+}*/
+
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int array[] = { 123, 2, 60, 100, 435, 0, 7 };
+    int* max_ptr = array;
+
+    for (int& i : array) {
+        if (i > *max_ptr) {
+            max_ptr = &i;
+        }
+    }
+
+    cout << *max_ptr;
 }
